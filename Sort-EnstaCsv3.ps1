d7041533e7e26b5d12607114c2d7531d49aed0f0
s-shiraha <eureka.anemone7@gmail.com>
2025-08-16 14:11:32 +0900

    （仕様変更） ・パラメータ名を変更 　StartXをStartingXに変更 　StartZをStartingZに変更 ・StartColumnをStartXに変更し、列番号ではなくX座標に変更

--- Sort-EnstaCsv3.ps1
+++ Sort-EnstaCsv3.ps1
@@ -1,9 +1,10 @@
 ﻿function Sort-EnstaCsv3 {
     param(
+        [parameter(Mandatory)][int]$StartingX, 
+        [parameter(Mandatory)][int]$StartingZ, 
         [parameter(Mandatory)][int]$StartX, 
-        [parameter(Mandatory)][int]$StartZ, 
-        [switch]$ShowStatistics = $false, 
-        [int]$StartColumn = 128
+        [switch]$ShowStatistics = $false
+        
     )
 
     $TargetFile = "${HOME}\pwsh\image_and_csv\nazuna3_MC.csv"
@@ -33,53 +34,54 @@
                 }, Name | `
                     Sort-Object -Property Count | `
                         Format-Table -AutoSize
-    }
+    }else {
+        #Columnを降順で走査
+        #for($j = $TargetWidth; $j -gt 0; $j--){
+        for($j = $StartX - $StartingX +$TargetWidth; $j -gt 0; $j--){
+            #該当列のブロック総数を表示
+            $CsvContent."${j}" | `
+                ForEach-Object -Process {
+                    $_ -replace "(\(明\)|\(標準\)|\(暗\))", ""
+                } | `
+                    Group-Object -NoElement | `
+                        Sort-Object -Property Count
+            "" | Out-Host
 
-    #Columnを降順で走査
-    for($j = $TargetWidth; $j -gt 0; $j--){
-        #該当列のブロック総数を表示
-        $CsvContent."${j}" | `
-            ForEach-Object -Process {
-                $_ -replace "(\(明\)|\(標準\)|\(暗\))", ""
-            } | `
-                Group-Object -NoElement | `
-                    Sort-Object -Property Count
-        "" | Out-Host
-
-        #ブロックを置き始めるy座標を計算
-        $MaxHeight = 0
-        $MinHeight = 0
-        $Height = 0
-        for($i = $TargetWidth; $i -gt 0; $i--){
-            #(明)なら$i - 1のブロックのy座標が-1
-            if($CsvContent."${j}"[$i] -match "\(明\)$"){
-                $Height--
-                if($MinHeight -gt $Height){$MinHeight = $Height}
-            }
-            #(暗)なら$i - 1のブロックのy座標が+1
-            if($CsvContent."${j}"[$i] -match "\(暗\)"){
-                $Height++
-                if($MaxHeight -lt $Height){$MaxHeight = $Height}
-            }
-            #(標準)なら$i - 1のブロックのy座標が+0、何もしない
-        }
-        "X:$($j - $TargetWidth + $StartX)`tStartY:$($StartY - $MinHeight)" | Out-Host
-        "" | Out-Host
-        
-        for($i = $TargetWidth - 1; $i -ge 0; $i--){
-            #置くブロック名を表示
-            $Y_Delta = "Y_Delta = +0"
-            if($i -ne $TargetWidth - 1){
-                if($CsvContent."${j}"[$i + 1] -match "\(明\)$"){
-                    $Y_Delta = "Y_Delta = -1"
-                } elseif($CsvContent."${j}"[$i + 1] -match "\(暗\)$"){
-                    $Y_Delta = "Y_Delta = +1"
+            #ブロックを置き始めるy座標を計算
+            $MaxHeight = 0
+            $MinHeight = 0
+            $Height = 0
+            for($i = $TargetWidth; $i -gt 0; $i--){
+                #(明)なら$i - 1のブロックのy座標が-1
+                if($CsvContent."${j}"[$i] -match "\(明\)$"){
+                    $Height--
+                    if($MinHeight -gt $Height){$MinHeight = $Height}
                 }
+                #(暗)なら$i - 1のブロックのy座標が+1
+                if($CsvContent."${j}"[$i] -match "\(暗\)"){
+                    $Height++
+                    if($MaxHeight -lt $Height){$MaxHeight = $Height}
+                }
+                #(標準)なら$i - 1のブロックのy座標が+0、何もしない
             }
-            "$($CsvContent."${j}"[$i])`tX:$($StartX + $j - $TargetWidth)`tZ:$($StartZ + $i - $TargetWidth)`t${Y_Delta}" | Out-Host
-            Read-Host
+            "X:$($j - $TargetWidth + $StartingX)`tStartY:$($StartY - $MinHeight)" | Out-Host
+            "" | Out-Host
+            
+            for($i = $TargetWidth - 1; $i -ge 0; $i--){
+                #置くブロック名を表示
+                $Y_Delta = "Y_Delta = +0"
+                if($i -ne $TargetWidth - 1){
+                    if($CsvContent."${j}"[$i + 1] -match "\(明\)$"){
+                        $Y_Delta = "Y_Delta = -1"
+                    } elseif($CsvContent."${j}"[$i + 1] -match "\(暗\)$"){
+                        $Y_Delta = "Y_Delta = +1"
+                    }
+                }
+                "$($CsvContent."${j}"[$i])`tX:$($StartingX + $j - $TargetWidth)`tZ:$($StartingZ + $i - $TargetWidth)`t${Y_Delta}" | Out-Host
+                Read-Host
+            }
         }
     }
 }
 
-Sort-EnstaCsv3 -StartX -193 -StartZ -65 -StartColumn 128 -ShowStatistics
+Sort-EnstaCsv3 -StartingX -193 -StartingZ -65 -StartX -194
